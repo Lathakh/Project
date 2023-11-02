d863e38a5750d956b90931642984ab7ec32b5f97
latha <lathakh2018@gmail.com>
2023-11-02 19:16:28 +0530

    updated component

--- src/DimondPricePrediction/components/data_ingestion.py
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -1,9 +1,22 @@
 #  To load the data  create class
 
+import pandas as pd
+import numpy as np
+from src.DimondPricePrediction.logger import logging
+from src.DimondPricePrediction.exceptions import customexception
+
+from sklearn.model_selection import  train_test_split
+from datasclasses import dataclass
+from pathlib import Path
+
 class DataIngestion:
     def __init__(self):
-        pass
+       
 
     def initiate_data_ingestion(self):
-        pass
-    
+        logging.info("data ingestion stated")
+
+        try:
+            pd.read_csv(F:\ineauron_course\ML_PROJECTS\Project\notebooks)
+        except Exception as e:
+            pass                
