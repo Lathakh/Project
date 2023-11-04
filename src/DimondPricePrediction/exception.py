66d671fc989fd48ba0b7bc0a46e167f414d71a28
latha <lathakh2018@gmail.com>
2023-11-04 14:25:47 +0530

    updated training pipeline

--- src/DimondPricePrediction/exception.py
+++ src/DimondPricePrediction/exception.py
@@ -4,12 +4,11 @@ import sys
 class customexception(Exception):
     def __init__(self,error_message,error_details:sys):
         self.error_message = error_message
-        _,_,exc_tb = error_details.exc_info()
-        
+        _,_,exc_tb = error_details.exc_info() # in which line, message,files we are getting error exc_info() gives the execution traceback
         self.lineno=exc_tb.tb_lineno
         self.file_name=exc_tb.tb_frame.f_code.co_filename 
     
-    def __str__(self):
+    def __str__(self): # string that print the custom error 
         return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
         self.file_name, self.lineno, str(self.error_message))
         
